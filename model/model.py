810a3f5f3363b6b750f9d2ff5057404263474934
kruksik <maciej.krakowiak17@gmail.com>
2021-08-10 09:23:33 +0200

    move stackedwidget logit to model

--- model/model.py
+++ model/model.py
@@ -1,21 +1,21 @@
 
-from PySide6.QtWidgets import QWidget,QPushButton
+from PySide6.QtWidgets import QWidget, QPushButton
 from view.view import View
 
 
 class Model:
     def __init__(self) -> None:
         self.all_menu_buttons = []
-        
-        
-    
-    def get_all_menu_buttons(self,obj):
+
+    def get_all_menu_buttons(self, obj):
         for button in obj.findChildren(QPushButton):
             self.all_menu_buttons.append(button)
-        
-            
-    
-         
-    
-    
-        
+
+    def check_which_slide(self, obj):
+        slides_and_pages = {
+            'btn_home': 0,
+            'btn_widgets': 1,
+            'btn_new': 2
+        }
+        slidename = obj.objectName()
+        return slides_and_pages[slidename]
--- view/view.py
+++ view/view.py
@@ -1,35 +1,30 @@
 
 from view.ui_main import Ui_MainWindow
-from PySide6.QtWidgets import QMainWindow,QGraphicsDropShadowEffect, QSizeGrip
-from PySide6.QtCore import QPropertyAnimation,QEasingCurve, Qt
+from PySide6.QtWidgets import QMainWindow, QGraphicsDropShadowEffect, QSizeGrip
+from PySide6.QtCore import QPropertyAnimation, QEasingCurve, Qt
 from PySide6.QtGui import QColor, QIcon
-from view.custom_modules import SlidingStackedWidget,Splashscreen
-
-
-
+from view.custom_modules import SlidingStackedWidget, Splashscreen
 
 
 class View(QMainWindow):
-    def __init__(self,model) -> None:
-        super(View,self).__init__()
+    def __init__(self, model) -> None:
+        super(View, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        
+
         self.model = model
-        
+
         self.setWindowTitle("Kruksik QT Layout")
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
-        
+
         self.grip = QSizeGrip(self.ui.resizeicon)
         self.grip.setStyleSheet('background: transparent')
-        
+
         self.splashscreen = Splashscreen(self)
- 
-        
-    
-    def toggleMenu(self,status):
-        
+
+    def toggleMenu(self, status):
+
         if self.sender().objectName() == 'ToggleButton':
             layout = self.ui.LeftMenuFrame
             maxExtend = 160
@@ -38,7 +33,7 @@ class View(QMainWindow):
             layout = self.ui.ExtraLeftMenuFrame
             maxExtend = 160
             standard = 0
-            
+
         if not status:
             width = layout.width()
 
@@ -47,49 +42,44 @@ class View(QMainWindow):
             else:
                 widthToExtend = standard
 
-            self.animation = QPropertyAnimation(layout,b'minimumWidth')
+            self.animation = QPropertyAnimation(layout, b'minimumWidth')
             self.animation.setDuration(500)
             self.animation.setStartValue(width)
             self.animation.setEndValue(widthToExtend)
             self.animation.setEasingCurve(QEasingCurve.InOutQuart)
             self.animation.start()
-            
-    def enable_shadow_effect(self,widget,blur,xoff,yoff):
+
+    def enable_shadow_effect(self, widget, blur, xoff, yoff):
         shadow = QGraphicsDropShadowEffect(self)
         shadow.setBlurRadius(blur)
         shadow.setXOffset(xoff)
         shadow.setYOffset(yoff)
-        shadow.setColor(QColor(0,0,0,80))
+        shadow.setColor(QColor(0, 0, 0, 80))
         widget.setGraphicsEffect(shadow)
-        
-        
-    ###### Event func ##### 
-  
 
-        
+    ###### Event func #####
+
     def maximize_windowsize(self):
         if not self.isMaximized():
             self.showMaximized()
             self.ui.CentralAppMargins.setContentsMargins(0, 0, 0, 0)
             self.ui.maximizeButton.setToolTip("Restore")
-            self.ui.maximizeButton.setIcon(QIcon(u":/icons/icons/icon_restore.png"))
+            self.ui.maximizeButton.setIcon(
+                QIcon(u":/icons/icons/icon_restore.png"))
             self.ui.resizeicon.hide()
-           
+
         else:
             self.showNormal()
             self.resize(self.width()+1, self.height()+1)
             self.ui.CentralAppMargins.setContentsMargins(10, 10, 10, 10)
             self.ui.maximizeButton.setToolTip("Maximize")
-            self.ui.maximizeButton.setIcon(QIcon(u":/icons/icons/icon_maximize.png"))
+            self.ui.maximizeButton.setIcon(
+                QIcon(u":/icons/icons/icon_maximize.png"))
             self.ui.resizeicon.show()
-    
-    def mousePressEvent(self,event):
+
+    def mousePressEvent(self, event):
         self.dragPos = event.globalPos()
-            
-  
-            
-    
-        
+
     def replaceWidgetsToCustom(self):
         self.ui.stackedWidget.deleteLater()
         self.ui.stackedWidget = SlidingStackedWidget()
@@ -98,12 +88,9 @@ class View(QMainWindow):
         self.ui.stackedWidget.addWidget(self.ui.home_page)
         self.ui.stackedWidget.addWidget(self.ui.next_page)
         self.ui.stackedWidget.addWidget(self.ui.title_page)
-        
+
         self.ui.stackedWidget.setCurrentWidget(self.ui.home_page)
-  
-    def changePage(self,button):
-        if button.objectName() == 'btn_home': self.ui.stackedWidget.slidetowidget(0)
-        elif button.objectName() == 'btn_widgets' : self.ui.stackedWidget.slidetowidget(1)
-        elif button.objectName() == 'btn_new' : self.ui.stackedWidget.slidetowidget(2)
-        else:
-            pass
+
+    def changePage(self, button):
+        slideto = self.model.check_which_slide(button)
+        self.ui.stackedWidget.slidetowidget(slideto)
